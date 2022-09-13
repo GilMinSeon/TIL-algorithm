a81f44632b15d3e484231109a0031bbf024d0797
GilMinSeon <77832207+GilMinSeon@users.noreply.github.com>
2022-09-13 20:28:58 +0900

    Update 01.백준 10828번-스택.py

--- 03.스택/01.백준 10828번-스택.py
+++ 03.스택/01.백준 10828번-스택.py
@@ -6,7 +6,7 @@ n = int(input)
 stk = []
 for _ in range(n):
     a = sys.stdin.readline().strip()
-    if a[:4] == 'push':
+    if a[:4] == 'push':     # 이렇게 할게 아니라 a[0] 하면 해결됨!!!
         num = a[5:]
         stk.append(num)
     elif a == 'pop':
