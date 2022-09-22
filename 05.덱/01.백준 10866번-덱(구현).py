f597d0ce416a6d4939ae9241ff58b76d4ad3497a
GilMinSeon <77832207+GilMinSeon@users.noreply.github.com>
2022-09-23 07:40:26 +0900

    Create 01.백준 10866번-덱(구현).py

---
+++ 05.덱/01.백준 10866번-덱(구현).py
@@ -0,0 +1,27 @@
+# youtube 승욱은 풀이 참고
+from collections import deque
+import sys
+input = sys.stdin.readline
+
+deq = deque()
+
+n = int(input())
+for _ in range(n):
+    qry = input().split()
+    if qry[0] == 'push_front':
+        deq.appendleft(qry[1])
+    elif qry[0] == 'push_back':
+        deq.append(qry[1])
+    elif qry[0] == 'pop_front':
+        print(deq.popleft() if deq else -1)
+    elif qry[0] == 'pop_back':
+        print(deq.pop() if deq else -1)
+    elif qry[0] == 'size':
+        print(len(deq))
+    elif qry[0] == 'empty':
+        print(0 if deq else 1)
+    elif qry[0] == 'front':
+        print(deq[0] if deq else -1)
+    elif qry[0] == 'back':
+        print(deq[-1] if deq else -1)
+
