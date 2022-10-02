df93b9888287840fc0cde5e7d790ca30c08ec3ba
GilMinSeon <77832207+GilMinSeon@users.noreply.github.com>
2022-10-02 21:51:23 +0900

    Create 07. 리트코드 24번 - 노드의 페어스왑.py

---
+++ 06.연결리스트/07. 리트코드 24번 - 노드의 페어스왑.py
@@ -0,0 +1,46 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def swapPairs(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        
+        
+        
+        '''
+        # <<<<<내가 1시간 반동안 고민한 코드>>>>> => 어떻게해서든 풀긴풀었다! 통과함
+        if not head:
+            return head
+        
+        if not head.next:
+            return head
+        
+        # 위에 두 개 합친 코드 구현하기!!!
+        dummy = ListNode(next = head)
+        
+        prev, curr = head, head.next
+        dummy.next = curr
+        
+        while True:
+            
+            nxt = curr.next
+            
+            curr.next = prev
+            
+            prev.next = nxt
+            
+            if nxt == None:
+                return dummy.next
+            else:
+                if nxt.next == None:
+                    return dummy.next
+                else:
+                    prev.next = nxt.next #### 이거를 못해서!!!!
+                    prev, curr = nxt, nxt.next
+        '''
+
+                
+                
+            
+        
