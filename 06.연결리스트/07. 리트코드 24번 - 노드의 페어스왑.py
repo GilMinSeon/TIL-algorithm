fe52117e64d75bf08c98737d76cb39f452898745
GilMinSeon <77832207+GilMinSeon@users.noreply.github.com>
2022-10-02 21:57:29 +0900

    Update 07. 리트코드 24번 - 노드의 페어스왑.py

--- 06.연결리스트/07. 리트코드 24번 - 노드의 페어스왑.py
+++ 06.연결리스트/07. 리트코드 24번 - 노드의 페어스왑.py
@@ -6,10 +6,8 @@
 class Solution:
     def swapPairs(self, head: Optional[ListNode]) -> Optional[ListNode]:
         
-        
-        
-        '''
-        # <<<<<내가 1시간 반동안 고민한 코드>>>>> => 어떻게해서든 풀긴풀었다! 통과함
+
+        # <<<<<내가 1시간 반동안 고민한 코드>>>>> ==> 어떻게해서든 풀긴풀어서 통과함!!! 이제는 다른 풀이 보고 최적화시키기!!
         if not head:
             return head
         
@@ -30,15 +28,21 @@ class Solution:
             
             prev.next = nxt
             
-            if nxt == None:
+            # if nxt == None:
+            #     return dummy.next
+            # else:
+            #     if nxt.next == None:
+            #         return dummy.next
+            #     else:
+            #         prev.next = nxt.next #### 이거를 못해서!!!!
+            #         prev, curr = nxt, nxt.next
+
+            if nxt == None or nxt.next == None:
                 return dummy.next
             else:
-                if nxt.next == None:
-                    return dummy.next
-                else:
-                    prev.next = nxt.next #### 이거를 못해서!!!!
-                    prev, curr = nxt, nxt.next
-        '''
+                prev.next = nxt.next #### 이거를 못해서!!!!
+                prev, curr = nxt, nxt.next
+
 
                 
                 
