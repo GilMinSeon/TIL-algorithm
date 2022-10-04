e10d9fa33ab3d6c855f160f528e6013666a683d0
GilMinSeon <77832207+GilMinSeon@users.noreply.github.com>
2022-10-04 19:49:17 +0900

    Update 07. 리트코드 24번 - 노드의 페어스왑.py

--- 06.연결리스트/07. 리트코드 24번 - 노드의 페어스왑.py
+++ 06.연결리스트/07. 리트코드 24번 - 노드의 페어스왑.py
@@ -6,7 +6,28 @@
 class Solution:
     def swapPairs(self, head: Optional[ListNode]) -> Optional[ListNode]:
         
-
+        #neetcode
+        dummy = ListNode(0, head)
+        prev, curr = dummy, head
+        
+        while curr and curr.next:
+            # 1) 선언 => save pointers
+            nxtPair = curr.next.next
+            second = curr.next
+            
+            # 2) next들을 옮겨주기 => reverse this pair
+            second.next = curr
+            curr.next = nxtPair
+            prev.next = second
+            
+            # 3) 좌표 shift => update pointers ** prev->curr!
+            prev = curr #!!!curr가 nxtPair 바로 뒤자리로 위에서 이동했기때문에 prev에댜가 curr을 넣어줄 수 있는것!!!
+            curr = nxtPair
+            
+        return dummy.next
+        
+        
+        '''
         # <<<<<내가 1시간 반동안 고민한 코드>>>>> ==> 어떻게해서든 풀긴풀어서 통과함!!! 이제는 다른 풀이 보고 최적화시키기!!
         if not head:
             return head
@@ -42,7 +63,7 @@ class Solution:
             else:
                 prev.next = nxt.next #### 이거를 못해서!!!!
                 prev, curr = nxt, nxt.next
-
+        '''
 
                 
                 
