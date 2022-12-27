40d6ab04a76603397cb3dbcabb6f36c0c1da34d5
GilMinSeon <littor1886@gmail.com>
2022-12-28 07:53:15 +0900

    자료구조-힙

---
+++ 02.자료구조1/02.스택/백준11279번-최대 힙 copy.py
@@ -0,0 +1,15 @@
+import heapq as hq
+import sys
+input = sys.stdin.readline
+n = int(input())
+a = []
+for _ in range(n):
+    x = int(input())
+    if x == 0:
+        if len(a) == 0:
+            print('0')
+        else:
+            print(-hq.heappop(a)) # 음수화
+    else:
+        hq.heappush(a, -x) # 음수화
+
---
+++ 02.자료구조1/02.스택/백준1927번-최소 힙.py
@@ -0,0 +1,23 @@
+'''
+만약 x가 자연수라면 배열에 x라는 값을 넣는(추가하는) 연산이고, 
+x가 0이라면 배열에서 가장 작은 값을 출력하고 그 값을 배열에서 제거하는 경우이다. 
+x는 231보다 작은 자연수 또는 0이고, 음의 정수는 입력으로 주어지지 않는다.
+
+입력에서 0이 주어진 횟수만큼 답을 출력한다. 
+만약 배열이 비어 있는 경우인데 가장 작은 값을 출력하라고 한 경우에는 0을 출력하면 된다.
+'''
+import heapq as hq
+import sys
+input = sys.stdin.readline
+n = int(input())
+a = []
+for _ in range(n):
+    x = int(input())
+    if x == 0:
+        if len(a) == 0:
+            print('0')
+        else:
+            print(hq.heappop(a))
+    else:
+        hq.heappush(a, x)
+
